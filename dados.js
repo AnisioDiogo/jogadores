ffa1baacb01f2ae4f35b9e5d26ce70a7bf3f311e
Anisio Diogo <168247565+AnisioDiogo@users.noreply.github.com>
2024-09-08 20:41:26 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Neymar Junior",
+        descricao: "Neymar da Silva Santos Junior, mais conhecido como Neymar",
+        link: "https://pt.wikipedia.org/wiki/Neymar",
+        tags: "Futebol jogador11 Brasileiro"
+    },
+    {
+        titulo: "Lionel Messi",
+        descricao: "Lionel Messi é considerado um dos maiores jogadores de futebol",
+        link: "https://pt.wikipedia.org/wiki/Lionel_Messi",
+        tags: "Futebol  jogador10 Argentino"
+    },
+    {
+        titulo: "Cristiano Ronaldo",
+        descricao: "Cristiano Ronaldo é outro ícone do futebol mundial",
+        link: "https://pt.wikipedia.org/wiki/Cristiano_Ronaldo",
+        tags: "Futebol jogador7  Português"
+    }
+];
